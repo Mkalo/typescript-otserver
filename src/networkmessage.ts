ffb84a1e51050b7f8b1332f21c1f119216d09383
Mkalo <mkalo@xtibia.com>
2017-02-23 19:32:50 -0300

    Update tabbing

--- src/networkmessage.ts
+++ src/networkmessage.ts
@@ -1,88 +1,88 @@
 import { NumericType } from "./ctypes";
 
 export class NetworkMessage {
-	dataBuffer: Buffer;
-	position: number;
+    dataBuffer: Buffer;
+    position: number;
 
-	constructor(size?: number) {
-		this.dataBuffer = Buffer.alloc(size || 24590, 0, "utf8");
-		this.position = 0;
-	}
+    constructor(size?: number) {
+        this.dataBuffer = Buffer.alloc(size || 24590, 0, "utf8");
+        this.position = 0;
+    }
 
-	getLength(): number {
-		return this.position;
-	}
+    getLength(): number {
+        return this.position;
+    }
 
-	getOutputBuffer(): Buffer {
-		return this.dataBuffer;
-	}
+    getOutputBuffer(): Buffer {
+        return this.dataBuffer;
+    }
 
-	canAdd(bytes: number): boolean {
-		return this.position + bytes - 1 < this.dataBuffer.length;
-	}
+    canAdd(bytes: number): boolean {
+        return this.position + bytes - 1 < this.dataBuffer.length;
+    }
 
-	addUInt8(value: number) {
-		if (!this.canAdd(1)) {
-			return;
-		}
+    addUInt8(value: number) {
+        if (!this.canAdd(1)) {
+            return;
+        }
 
-		this.position = this.dataBuffer.writeUInt8(NumericType.getUInt8(value), this.position);
-	}
+        this.position = this.dataBuffer.writeUInt8(NumericType.getUInt8(value), this.position);
+    }
 
-	addUInt16(value: number) {
-		if (!this.canAdd(2)) {
-			return;
-		}
+    addUInt16(value: number) {
+        if (!this.canAdd(2)) {
+            return;
+        }
 
-		this.position = this.dataBuffer.writeUInt16LE(NumericType.getUInt16(value), this.position);
-	}
+        this.position = this.dataBuffer.writeUInt16LE(NumericType.getUInt16(value), this.position);
+    }
 
-	addUInt32(value: number) {
-		if (!this.canAdd(4)) {
-			return;
-		}
+    addUInt32(value: number) {
+        if (!this.canAdd(4)) {
+            return;
+        }
 
-		this.position = this.dataBuffer.writeUInt32LE(NumericType.getUInt32(value), this.position);
-	}
+        this.position = this.dataBuffer.writeUInt32LE(NumericType.getUInt32(value), this.position);
+    }
 
-	addInt8(value: number) {
-		if (!this.canAdd(1)) {
-			return;
-		}
+    addInt8(value: number) {
+        if (!this.canAdd(1)) {
+            return;
+        }
 
-		this.position = this.dataBuffer.writeInt8(NumericType.getInt8(value), this.position);
-	}
+        this.position = this.dataBuffer.writeInt8(NumericType.getInt8(value), this.position);
+    }
 
-	addInt16(value: number) {
-		if (!this.canAdd(2)) {
-			return;
-		}
+    addInt16(value: number) {
+        if (!this.canAdd(2)) {
+            return;
+        }
 
-		this.position = this.dataBuffer.writeInt16LE(NumericType.getInt16(value), this.position);
-	}
+        this.position = this.dataBuffer.writeInt16LE(NumericType.getInt16(value), this.position);
+    }
 
-	addInt32(value: number) {
-		if (!this.canAdd(4)) {
-			return;
-		}
+    addInt32(value: number) {
+        if (!this.canAdd(4)) {
+            return;
+        }
 
-		this.position = this.dataBuffer.writeInt32LE(NumericType.getInt32(value), this.position);
-	}
+        this.position = this.dataBuffer.writeInt32LE(NumericType.getInt32(value), this.position);
+    }
 
-	addString(value: string) {
-		let size: number = value.length;
-		if (!this.canAdd(size + 2) || size > 8192) {
-			return;
-		}
+    addString(value: string) {
+        let size: number = value.length;
+        if (!this.canAdd(size + 2) || size > 8192) {
+            return;
+        }
 
-		this.addUInt16(size);
-		this.position += this.dataBuffer.write(value, this.position, value.length);
-	}
+        this.addUInt16(size);
+        this.position += this.dataBuffer.write(value, this.position, value.length);
+    }
 
-	addPaddingBytes(bytes: number) {
-	    for (let i = 0; i < bytes; i++) {
-			this.addUInt8(0x33);
-		}
-	}
+    addPaddingBytes(bytes: number) {
+        for (let i = 0; i < bytes; i++) {
+            this.addUInt8(0x33);
+        }
+    }
 
 }
