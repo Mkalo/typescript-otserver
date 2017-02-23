443f400b532c1182fef05bf64b74c1b138788eb3
Tomasz Martyński <tomek@martynski.pl>
2017-02-24 00:34:02 +0100

    added config loading

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import { Otserv } from "./otserv";
-Otserv.start();
+let config = null;
+try {
+	config = require('../config');
+} catch (e) {
+	console.error("Copy `config.js.sample` to `config.js` and edit it before starting the server.");
+	process.exit();
+}
+
+const otserv = new Otserv(config);
+otserv.start();
