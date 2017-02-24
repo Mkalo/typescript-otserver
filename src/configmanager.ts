894df9bb2530b9ae33ffa2bb7f10177e4ccb74b0
Tomasz Martyński <tomek@martynski.pl>
2017-02-24 22:20:47 +0100

    added default config

--- src/configmanager.ts
+++ src/configmanager.ts
@@ -1,23 +1,34 @@
+import * as deepExtend from 'deep-extend';
+
+class DBConfig {
+	type: string = 'mysql';
+	host: string = '127.0.0.1';
+	port: number = 3306;
+	user: string = 'root';
+	password: string = '';
+	dbName: string = 'some_ots';
+}
+
+class ConnectionConfig {
+	host: string = '127.0.0.1';
+	loginPort: number = 7171;
+	gamePort: number = 7172;
+}
+
+class WorldConfig {
+	type: string = 'pvp';
+	name: string = 'Typescripten';
+	motd: string = 'Message of the day!';
+}
+
 class Config {
-    world: {
-        type: string;
-        name: string;
-        motd: string;
-    };
+	constructor(obj: Object) {
+		deepExtend(this, obj);
+	}
 
-    connection: {
-        ip: string;
-        loginPort: number;
-        gamePort: number;
-    }
-
-    db: {
-        type: string;
-        host: string;
-        port: number;
-        user: string;
-        password: string;
-    };
+    world: WorldConfig = new WorldConfig();
+    connection: ConnectionConfig = new ConnectionConfig();
+    db: DBConfig = new DBConfig();
 }
 
 export class ConfigManager {
@@ -27,12 +38,12 @@ export class ConfigManager {
     public load(): boolean {
         let loadedConfig;
         try {
-            loadedConfig = require('../config.js');
+            loadedConfig = require('../config.js').default;
         } catch (e) {
             return false;
         }
 
-        this.config = loadedConfig.default;
+        this.config = new Config(loadedConfig);
         console.log(typeof(this.config.db.port) + ", " + this.config.db.port);
         return true;
     }
