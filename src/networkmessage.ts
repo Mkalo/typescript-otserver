ddf1a86595ce76dc68446e6c72e64f55a0ee1b13
Tomasz Martyński <tomek@martynski.pl>
2017-02-25 14:31:25 +0100

    Updated NetworkMessage

--- src/networkmessage.ts
+++ src/networkmessage.ts
@@ -4,6 +4,22 @@ export const NETWORKMESSAGE_MAXSIZE = 24590;
 
 export class NetworkMessage extends Binary {
 
+	constructor(bufferOrLength?: Buffer | number) {
+		if (typeof bufferOrLength !== "undefined") {
+			super(bufferOrLength);
+			return;
+		}
+
+		super(NETWORKMESSAGE_MAXSIZE);
+	}
+
+	public getBuffer(): Buffer {
+		const bufferLength = this.getPosition();
+		const buffer: Buffer = new Buffer(bufferLength);
+		this.getBuffer().copy(buffer, 0, 0, bufferLength);
+        return buffer;
+    }
+
     public addPaddingBytes(bytes: number) {
         for (let i = 0; i < bytes; i++) {
             this.addUInt8(0x33);
