e3e529b4c12aeb5a0856ba757c11a7eb6376af8e
Tomasz Martyński <tomek@martynski.pl>
2017-02-26 11:10:50 +0100

    Added overloaded constructors to Position class

--- src/position.ts
+++ src/position.ts
@@ -3,9 +3,27 @@ export class Position {
 	private y: number;
 	private z: number;
 
-	constructor(x, y, z) {
+	constructor(position: Position);
+	constructor(x: number, y: number, z: number);
+	constructor(p1: any, p2?: any, p3?: any) {
+		if (p1 instanceof Position)
+			return this.constructor1(p1);
+
+		if (typeof p1 === "number" && typeof p2 === "number" && typeof p3 === "number")
+			return this.constructor2(p1, p2, p3);
+	}
+
+	private constructor1(position: Position) {
+		this.x = position.x;
+		this.y = position.y;
+		this.z = position.z;
+		return this;
+	}
+
+	private constructor2(x: number, y: number, z: number) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		return this;
 	}
 }
