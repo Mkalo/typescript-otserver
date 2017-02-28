d2a3345fd9fa14f4947cf377466ea9eda2205c2e
Tomasz Martyński <tomek@martynski.pl>
2017-02-28 09:08:23 +0100

    Changed Bignumber library because old one sucked dick + changed rsa to work with it

--- src/rsa.ts
+++ src/rsa.ts
@@ -1,4 +1,4 @@
-import * as BigInteger from "big-integer";
+import * as bignum from 'bignum';
 
 export class RSA {
 
@@ -6,43 +6,32 @@ export class RSA {
 	private static defaultq: string = "7630979195970404721891201847792002125535401292779123937207447574596692788513647179235335529307251350570728407373705564708871762033017096809910315212884101";
 	private static instance: RSA = new RSA();
 
-	private p: BigInteger;
-	private q: BigInteger;
-	private n: BigInteger;
-	private d: BigInteger;
-	private e: BigInteger;
-	private dmp1: BigInteger;
-	private dmq1: BigInteger;
-	private coeff: BigInteger;
+	private n: bignum;
+	private d: bignum;
 
 	protected constructor() {
 		this.setRSA(RSA.defaultp, RSA.defaultq);
 	}
 
-	public setRSA(p: string, q: string) {
-		this.p = BigInteger(p);
-		this.q = BigInteger(q);
-		this.n = BigInteger(this.p.multiply(this.q).toString());
-		this.e = BigInteger(65537);
+	public setRSA(_p: string, _q: string) {
+		const p = bignum(_p);
+		const q = bignum(_q);
+		this.n = bignum(p.mul(q).toString());
+		const e = bignum(65537);
 
-		const p_1: BigInteger = this.p.subtract(1);
-		const q_1: BigInteger = this.q.subtract(1);
-		const pq_1: BigInteger = p_1.multiply(q_1);
+		const p_1: bignum = p.sub(1);
+		const q_1: bignum = q.sub(1);
+		const pq_1: bignum = p_1.mul(q_1);
 
-		this.d = this.e.modInv(pq_1);
-		this.dmp1 = this.d.mod(p_1);
-		this.dmq1 = this.d.mod(q_1);
-		this.coeff = this.q.modInv(this.p);
+		this.d = e.invertm(pq_1);
 	}
 
 	public decrypt(buffer: Buffer, start: number = 0): void {
 		const bufferToDecrypt: Buffer = new Buffer(128);
 		buffer.copy(bufferToDecrypt, 0, start, start + 128);
 
-		const c: BigInteger = BigInteger(bufferToDecrypt.toString("hex"), 16);
-		const m: BigInteger = c.modPow(this.d, this.n); // m = c^d mod n
-		const dec = m.toString(16);
-		const decrypted = Buffer.from(dec, "hex");
+		const bufferInt = bignum.fromBuffer(bufferToDecrypt);
+		const decrypted = bignum.powm(bufferInt, this.d, this.n).toBuffer();
 
 		let success = decrypted.length === 127;
 
