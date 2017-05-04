1e5bc05a9947a1b46a812e07ea201d412d67383f
elderapo <tomek@martynski.pl>
2017-05-05 01:37:48 +0200

    Typo fix

--- src/protocols/protocol.ts
+++ src/protocols/protocol.ts
@@ -33,8 +33,8 @@ export abstract class Protocol {
 	}
 
 	public onRecvMessage(msg: NetworkMessage): void {
-		if (this.isXTEAEncryptionEnabled && !this.xteaKey.decrypt(msg)) {
-			return
+		if (this.isXTEAEncryptionEnabled() && !this.xteaKey.decrypt(msg)) {
+			return;
 		}
 
 		return this.parsePacket(msg);
