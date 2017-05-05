70aeb3c85d69ede656e51bdbb9cfc3379385a7d7
elderapo <tomek@martynski.pl>
2017-05-05 02:45:18 +0200

    Temp workaround #8

--- src/xtea.ts
+++ src/xtea.ts
@@ -41,29 +41,34 @@ export class XTEA {
 	}
 
 	public decrypt(msg: NetworkMessage): boolean {
-		// if ((msg.getLength() - msg.getPosition()) % 8 !== 0)
-		// 	return false;
+		if ((msg.getLength() - msg.getPosition()) % 8 !== 0)
+			return false;
 
 		const buffer: Buffer = msg.getBuffer();
 		const messageLength: number = buffer.length;
 		let readPos: number = msg.getPosition();
 		while (readPos < messageLength) {
-			let v0: number = buffer.readUInt32LE(readPos);
-			let v1: number = buffer.readUInt32LE(readPos + 4);
+			try {
+				let v0: number = buffer.readUInt32LE(readPos);
+				let v1: number = buffer.readUInt32LE(readPos + 4);
 
-			let sum: number = 0xC6EF3720;
+				let sum: number = 0xC6EF3720;
 
-			for (let i = 32; --i >= 0;) {
-				v1 -= ((NumericType.getUInt32(v0 << 4) ^ (v0 >>> 5)) + v0) ^ (sum + this.key[(sum >> 11) & 3]);
-				v1 = NumericType.getUInt32(v1);
-				sum = NumericType.getUInt32(sum + XTEA.delta);
-				v0 -= ((NumericType.getUInt32(v1 << 4) ^ (v1 >>> 5)) + v1) ^ (sum + this.key[sum & 3]);
-				v0 = NumericType.getUInt32(v0);
+				for (let i = 32; --i >= 0;) {
+					v1 -= ((NumericType.getUInt32(v0 << 4) ^ (v0 >>> 5)) + v0) ^ (sum + this.key[(sum >> 11) & 3]);
+					v1 = NumericType.getUInt32(v1);
+					sum = NumericType.getUInt32(sum + XTEA.delta);
+					v0 -= ((NumericType.getUInt32(v1 << 4) ^ (v1 >>> 5)) + v1) ^ (sum + this.key[sum & 3]);
+					v0 = NumericType.getUInt32(v0);
+				}
+
+				buffer.writeUInt32LE(v0, readPos);
+				buffer.writeUInt32LE(v1, readPos + 4);
+				readPos += 8;
+			} catch (e) {
+				console.log('XTEA.decrypt throwed error :(');
+				return true;
 			}
-
-			buffer.writeUInt32LE(v0, readPos);
-			buffer.writeUInt32LE(v1, readPos + 4);
-			readPos += 8;
 		}
 
 		return true;
