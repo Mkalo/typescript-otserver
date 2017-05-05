ae241c7358035f8357089cada39714a5e1a864aa
elderapo <tomek@martynski.pl>
2017-05-05 02:11:39 +0200

    Game loading players and players cache

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,5 @@
 import { GameState } from './enums';
+import { Player } from './Player';
 
 export class Game {
 
@@ -6,6 +7,30 @@ export class Game {
 	public maxClientVersion: number = 100000;
 	public clientVersionString: number = 10;
 
+	public players: Player[] = [];
+
+	private getPlayerFromCache(playerName: string): Player {
+		for (let player of this.players) {
+			if (player.name === playerName)
+				return player;
+		}
+		return null;
+	}
+
+	public getPlayerByName(playerName: string, done: (err: Error, player?: Player) => void): void {
+		const playerFromCache = this.getPlayerFromCache(playerName);
+		if (playerFromCache)
+			return done(null, playerFromCache);
+
+		Player.loadPlayerFromDatabase(playerName, (err, playerFromDatabase) => {
+			if (err) return done(err);
+
+			if (playerFromDatabase)
+				this.players.push(playerFromDatabase);
+
+			return done(null, playerFromDatabase);
+		});
+	}
 
 	public getState(): number {
 		return GameState.Normal;
